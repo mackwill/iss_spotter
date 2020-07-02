ce4d7937843a1c6fb9836aa661602c36fcee37f1
William <mack.mindenhall@gmail.com>
2020-07-02 17:11:23 +0000

    Code cleanup

--- index.js
+++ index.js
@@ -1,40 +1,4 @@
-const {
-  fetchMyIP,
-  fetchCoordsByIP,
-  fetchISSFlyOverTimes,
-  nextISSTimesForMyLocation,
-} = require("./iss");
-
-// fetchMyIP((error, ip) => {
-//   if (error) {
-//     console.log("It didnt work! ", error);
-//     return;
-//   }
-
-//   console.log("It worked! Nicolas Cage has your IP: ", ip);
-// });
-
-// fetchCoordsByIP("24.202.7.129", (error, data) => {
-//   // fetchCoordsByIP("24.202.7.12228", (error, data) => {
-//   if (error) {
-//     console.log("Nicolas Cage found an error in your work.", error);
-//     return;
-//   }
-
-//   console.log(`Nicolas Cage found your coordinates at: `, data);
-// });
-
-// fetchISSFlyOverTimes({ lat: "45.35260", lon: "-73.73050" }, (error, desc) => {
-//   if (error) {
-//     console.log(`Nicolas Cage found an error in your request: ${error}`);
-//     return;
-//   }
-
-//   console.log(
-//     `Nicolas Cage predicts the ISS will pass over at these times: `,
-//     desc
-//   );
-// });
+const { nextISSTimesForMyLocation } = require("./iss");
 
 nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
